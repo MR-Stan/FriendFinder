b81b356c3268d51ffaebe3e0fb8c931ffb5e28a7
MR-Stan <michaelrstanley87@gmail.com>
2019-12-07 19:22:28 -0600

    completed survey form

--- app/public/survey.js
+++ app/public/survey.js
@@ -9,21 +9,23 @@ let questionBank = [
     "I see myself as disorganized, careless.",
     "I see myself as calm, emotionally stable.",
     "I see myself as conventional, uncreative."
-]
+];
+
+
+
+
 
 for (let i = 1; i < questionBank.length + 1; i++) {
     $('#questions').append('<hr>')
-    $("#questions").append("Question " + i + "<br>" + questionBank[i - 1]);
+    $("#questions").append("Question " + i + "<br>" + questionBank[i - 1]).append('<br>');
     $("#questions").append([
-        $('<div/>', { 'class': 'dropdown' }),
-        $('<button/>', { 'class': 'btn btn-secondary dropdown-toggle', 'type': 'button', 'id': 'dropdownMenuButton', 'data-toggle': 'dropdown', 'aria-haspopup': 'true', 'aria-expanded': 'false', 'text': '       ' }).append([
-            $('<div/>', { 'class': 'dropdown-menu', 'area-labelledby': 'dropdownMenuButton' }).append([
-                $('<a/>', { 'class': 'dropdown-item', 'href': '#', 'text': 'Strongly Agree', 'id': 'answer1', }),
-                $('<a/>', { 'class': 'dropdown-item', 'href': '#', 'text': 'Agree', 'id': 'answer2', }),
-                $('<a/>', { 'class': 'dropdown-item', 'href': '#', 'text': 'Meh', 'id': 'answer3', }),
-                $('<a/>', { 'class': 'dropdown-item', 'href': '#', 'text': 'Disagree', 'id': 'answer4', }),
-                $('<a/>', { 'class': 'dropdown-item', 'href': '#', 'text': 'Stronly Disagree', 'id': 'answer5', }),
-            ])
+        $('<select/>', { 'class': 'selectpicker' }).append([
+            $('<option/>', { 'text': ' ' }),
+            $('<option/>', { 'text': 'Strongly Agree', 'id': '5' }),
+            $('<option/>', { 'text': 'Agree', 'id': '4' }),
+            $('<option/>', { 'text': 'Neutral', 'id': '3' }),
+            $('<option/>', { 'text': 'Disagree', 'id': '2' }),
+            $('<option/>', { 'text': 'Strongly Disagree', 'id': '1' }),
         ])
     ])
 }
